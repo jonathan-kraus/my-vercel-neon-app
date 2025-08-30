16bba9bff6de99eec1645e629f659a2202e8619e
jonathan-kraus <jonathanckraus@gmail.com>
2025-08-30 17:23:23 -0400

    a1

--- app/page.tsx
+++ app/page.tsx
@@ -78,7 +78,7 @@ export default async function Home() {
   const prisma = new PrismaClient();
   const posts = await prisma.post.findMany({
     orderBy: { createdAt: 'desc' },
-    include: { published: true },
+    include: { author: "Jonathan" },
   });
   const users = await prisma.user.findMany({
     orderBy: { name: 'asc' },
