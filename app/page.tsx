b128f503b591be6e2bd40523ea0884646d5e04d8
jonathan-kraus <jonathanckraus@gmail.com>
2025-08-30 12:28:33 -0400

    Second from local

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ import { checkDbConnection } from "./db";
 const DATA = {
   title: "Vercel with Neon Postgres",
   description:
-    "A minimal template for Jonathan",
+    "A minimal template for Jonathan!",
   button: {
     text: "Deploy to Vercel with Neon",
     href: "https://vercel.com/new/clone?repository-url=https%3A%2F%2Fgithub.com%2Fneondatabase-labs%2Fvercel-marketplace-neon%2Ftree%2Fmain&project-name=my-vercel-neon-app&repository-name=my-vercel-neon-app&products=[{%22type%22:%22integration%22,%22integrationSlug%22:%22neon%22,%22productSlug%22:%22neon%22,%22protocol%22:%22storage%22}]",
