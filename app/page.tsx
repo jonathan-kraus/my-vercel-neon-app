6b760dc820a0db70c7aca3788ea16aa0542ccb2f
jonathan-kraus <jonathanckraus@gmail.com>
2025-08-31 20:23:54 -0400

    dc6

--- app/page.tsx
+++ app/page.tsx
@@ -116,12 +116,13 @@ export default async function Home() {
         <div className="flex justify-between items-center">
           <div>
             <span className="text-lg font-semibold">{post.title}</span>
+            <p>{post.content}</p>
             <p className="text-sm text-gray-600">By {post.author?.name || 'Unknown'} 
                 {myspace} on {new Date(post.createdAt).toLocaleDateString(
                 'en-US',
                 { year: 'numeric', month: '2-digit', day: '2-digit' }
-              )}</p>
-            <p>{post.content}</p>
+              )}
+            </p>
           </div>
           <form action={deletePost}>
             <input type="hidden" name="id" value={post.id} />
