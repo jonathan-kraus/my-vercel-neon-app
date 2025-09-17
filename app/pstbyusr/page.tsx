27b09dffd87a7cde6ef3e3150d7f0d138c0197ac
jonathan-kraus <jonathanckraus@gmail.com>
2025-09-16 20:54:46 -0400

    color2

--- app/pstbyusr/page.tsx
+++ app/pstbyusr/page.tsx
@@ -118,7 +118,7 @@ export default function BlogViewer() {
             <li
               key={post.id}
               className={`border-b pb-2 ${
-                index % 2 === 0 ? 'bg-gray-50' : 'bg-sky-50'
+                index % 2 === 0 ? 'bg-blue-50' : 'bg-sky-50'
               }`}
             >
               <span className="text-lg font-semibold">{post.title}</span>
