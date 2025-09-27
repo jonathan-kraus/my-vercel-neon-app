9dc7e10af8c0a6fd7cfeae60e6f1fb531524db9b
jonathan-kraus <jonathanckraus@gmail.com>
2025-09-26 23:31:07 -0400

    ac

---
+++ app/components/actions.tsx
@@ -0,0 +1,7 @@
+'use server';
+
+import { sendConfirmationEmail } from '../utils/sendemail';
+
+export async function triggerEmail() {
+  await sendConfirmationEmail('jonathanckraus@gmail.com', 'JKGMail');
+}
