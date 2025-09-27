be399312c2703334962094281d965bcb0d4d06c3
jonathan-kraus <jonathanckraus@gmail.com>
2025-09-26 21:46:35 -0400

    removed email

--- app/utils/email.ts
+++ app/utils/email.ts
@@ -5,18 +5,20 @@ const mailerSend = new MailerSend({
   apiKey: "mlsn.12387b37da7114b5f3b9228c6894f606b08a0619200d54e341b8138324a703ce"
 });
 
-const sentFrom = new Sender("you@yourdomain.com", "Your name");
-
+const sentFrom = new Sender("Jonathan@kraus.my.id", "Jonathan");
+// const sentFrom = new Sender("J@test-51ndgwv663dlzqx8.mlsender.net", "Jonathan");
+const jemail = "jonathanckraus@gmail.com";
 const recipients = [
-  new Recipient("your@client.com", "Your Client")
+  new Recipient(jemail, "JKGMail"),
 ];
 
 const emailParams = new EmailParams()
   .setFrom(sentFrom)
   .setTo(recipients)
   .setReplyTo(sentFrom)
-  .setSubject("This is a Subject")
-  .setHtml("<strong>This is the HTML content</strong>")
-  .setText("This is the text content");
+  .setSubject("Mail Success")
+  .setHtml(`<strong>Sent from my neonvercelJ ${jemail} app</strong>`)
+  .setText(`Sent from my neonvercelJ ${jemail} app`);
 
 await mailerSend.email.send(emailParams);
+console.log('✅ Email sent from utilssuccessfully to:', jemail);
