f0072fa1fec6850606c5de010a3a02d2357b7ffa
jonathan-kraus <jonathanckraus@gmail.com>
2025-10-04 13:42:08 -0400

    m1

--- app/actions/getWeather.ts
+++ app/actions/getWeather.ts
@@ -12,6 +12,7 @@ type WeatherResponse = {
   emailSent: boolean;
   lastEmailTimestamp: string | null;
 };
+console.log(`[getWeather] Server function triggered at ${new Date().toISOString()}`);
 
 const prisma = new PrismaClient();
 
