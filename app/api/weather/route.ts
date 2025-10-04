3bc607f7d2d33ff9502dcb57b54914d194d9091e
jonathan-kraus <jonathanckraus@gmail.com>
2025-10-04 00:38:16 -0400

    route

--- app/api/weather/route.ts
+++ app/api/weather/route.ts
@@ -1,9 +1,8 @@
-/**
- * @vercel/cron 0 13 * * *
- */
-
 import { getWeather } from '@/app/actions/getWeather';
+import { createLog } from '@/app/utils/db';
 
+  console.log('In api weather route');
+  createLog({authorId: 1101,title: 'cron',content: `Weather cron ran at ${new Date().toISOString()}`}).catch(console.error);
 export async function GET() {
   await getWeather();
   return new Response('Weather logged');
