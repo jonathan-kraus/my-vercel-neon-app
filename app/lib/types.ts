6680460c86f34773ba4f0296e54798b92b5c7e07
jonathan-kraus <jonathanckraus@gmail.com>
2025-10-07 16:46:27 -0400

    reset3

--- app/lib/types.ts
+++ app/lib/types.ts
@@ -1,8 +1,10 @@
+import { Prisma } from '@prisma/client'
+
 // lib/types.ts
 export type LogEvent = {
   severity: 'info' | 'warn' | 'error' | 'debug'
   source: string
   message: string
   requestId?: string
-  metadata?: Record<string, unknown>
+  metadata?: Prisma.JsonValue
 }
