b8dabae3c6ed707ad11312d9e3959f80ff20cfea
jonathan-kraus <jonathanckraus@gmail.com>
2025-10-09 00:05:55 -0400

    b7

--- app/components/CreatePostForm.tsx
+++ app/components/CreatePostForm.tsx
@@ -1,11 +1,11 @@
 'use client';
-import { useUser } from "@stackframe/react";
+//import { useUser } from "@stackframe/react";
 
 export default function CreatePostForm() {
   //const user = useUser();
   const user = { id: "J" }; // Mock user for testing purposes
   if (!user) {
-    return <p>"Please sign in to create a post."</p>; // early exit if not signed in
+    return <p>Please sign in to create a post.</p>; // early exit if not signed in
   }
 
   console.log("[CreatePostForm] Current user:", user); // runs only if user exists
