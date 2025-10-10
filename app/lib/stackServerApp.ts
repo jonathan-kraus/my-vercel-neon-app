fbc7e03a4010b2502194a10d00c4d02248539b4e
jonathan-kraus <jonathanckraus@gmail.com>
2025-10-10 00:36:27 -0400

    b7

--- app/lib/stackServerApp.ts
+++ app/lib/stackServerApp.ts
@@ -9,5 +9,5 @@ import { StackServerApp } from "@stackframe/stack";
 //   publishableClientKey: process.env.NEXT_PUBLIC_STACK_PUBLISHABLE_CLIENT_KEY!,
 //   tokenStore: "nextjs-cookie",
 // });
-// eslint-disable-next-line @next/next/no-server-import-in-client-component
+
 export const stackServerApp = {} as StackServerApp;
