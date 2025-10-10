28bc8ad13522c1564feb19dfd629ebd7c5547609
jonathan-kraus <jonathanckraus@gmail.com>
2025-10-10 14:57:43 -0400

    fix delete

--- app/actions/deletePost.ts
+++ app/actions/deletePost.ts
@@ -8,23 +8,13 @@ const requestId = crypto.randomUUID();
 export async function deletePost(formData: FormData) {
   const id = Number(formData.get('id'));
   const session = 1; // mock session
-await logEvent({
-  source: 'deletePost',
-  message: `Post will be deleted with ID: ${id}`,
-  requestId,
-  metadata: { userAction: 'delete' },
-});
+
   if (!session) throw new Error('Unauthorized');
   if (!id || isNaN(id)) throw new Error('Invalid post ID');
 
   await db.post.delete({
     where: { id },
   });
-  await logEvent({
-  source: 'deletePost',
-  message: `Post deleted with ID: ${id}`,
-  requestId,
-  metadata: { userAction: 'delete' },
-});
+
   //toast.success('Post deleted'); // Note: toast won't work in server actions  
 }
