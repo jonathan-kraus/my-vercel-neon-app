68ad4e604c3451b62a245d1d1cd647353a2c1bf8
jonathan-kraus <jonathanckraus@gmail.com>
2025-10-09 23:45:12 -0400

    a8

--- app/lib/stackServerApp.ts
+++ app/lib/stackServerApp.ts
@@ -1,8 +1,7 @@
-import { StackServerApp } from '@stackframe/stack';
+import "server-only";
+
+import { StackServerApp } from "@stackframe/stack";
 
 export const stackServerApp = new StackServerApp({
-  projectId: process.env.NEXT_PUBLIC_STACK_PROJECT_ID!,
-  publishableClientKey: process.env.NEXT_PUBLIC_STACK_PUBLISHABLE_CLIENT_KEY!,
-  //secretKey: process.env.STACK_SECRET_SERVER_KEY!,
-  tokenStore: 'nextjs-cookie',
+  tokenStore: "nextjs-cookie",
 });
