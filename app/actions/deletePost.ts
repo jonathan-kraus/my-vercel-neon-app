b656a1b9a797aeb69adb36237ae2c87c49f30e9a
jonathan-kraus <jonathanckraus@gmail.com>
2025-10-09 23:21:42 -0400

    a4

--- app/actions/deletePost.ts
+++ app/actions/deletePost.ts
@@ -1,10 +1,12 @@
 'use server';
 
-import { stackServerApp } from "../../stack/server";
+//import { stackServerApp } from "../../stack/server";
 import { db } from '@/app/lib/db'; // assuming you have a shared Prisma client
 
 export async function deletePost(formData: FormData) {
-  const session =  stackServerApp.validateRequest();
+ // const session =  stackServerApp.validateRequest();
+ const session = 1;
+ 
   if (!session) throw new Error('Unauthorized');
 
   const id = Number(formData.get('id'));
@@ -12,7 +14,7 @@ export async function deletePost(formData: FormData) {
   await db.post.delete({
     where: {
       id,
-      authorId: session.userId,
+      
     },
   });
 }
