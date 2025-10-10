640b472c2149917db10bbdc85cf74257ff89cea0
jonathan-kraus <jonathanckraus@gmail.com>
2025-10-10 12:59:01 -0400

    removing stack

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
-import Providers from './providers';
-import { StackTheme } from '@stackframe/stack';
+
+
 import SideNav from './components/SideNav';
 import ClientShell from '@/app/components/ClientShell';
 import { MockUserProvider } from './context/MockUserContext';
@@ -10,15 +10,15 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="en" className="dark">
       <body>
         <MockUserProvider>
-          <Providers>
-            <StackTheme>
+
+
               <div className="layout">
                 <SideNav />
                 <main className="flex-1">{children}</main>
               </div>
               <ClientShell />
-            </StackTheme>
-          </Providers>
+
+
         </MockUserProvider>
       </body>
     </html>
