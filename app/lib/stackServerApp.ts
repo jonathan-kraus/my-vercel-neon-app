fa04e4623548658b662fda040933e243dbfa5dc3
jonathan-kraus <jonathanckraus@gmail.com>
2025-10-09 23:30:57 -0400

    a6

--- app/lib/stackServerApp.ts
+++ app/lib/stackServerApp.ts
@@ -3,6 +3,6 @@ import { StackServerApp } from '@stackframe/stack';
 export const stackServerApp = new StackServerApp({
   projectId: process.env.NEXT_PUBLIC_STACK_PROJECT_ID!,
   publishableClientKey: process.env.NEXT_PUBLIC_STACK_PUBLISHABLE_CLIENT_KEY!,
-  secretKey: process.env.STACK_SECRET_SERVER_KEY!,
+  //secretKey: process.env.STACK_SECRET_SERVER_KEY!,
   tokenStore: 'nextjs-cookie',
 });
