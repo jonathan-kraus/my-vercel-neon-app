7efdd3ba9cfeb5706829236949c5229407ca0897
jonathan-kraus <jonathanckraus@gmail.com>
2025-10-10 02:10:24 -0400

    c9

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,27 +3,23 @@ import { StackTheme } from '@stackframe/stack';
 import SideNav from './components/SideNav';
 import ClientShell from '@/app/components/ClientShell';
 import { MockUserProvider } from './context/MockUserContext';
-
-
-
 import './globals.css';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" className="dark">
       <body>
-        <Providers>
-          <StackTheme>
-            <div className="layout">
-              <SideNav />
-              <main className="flex-1">{children}</main>
-            </div>
-            <ClientShell />
-            <MockUserProvider>
-              {children}
-            </MockUserProvider>
-          </StackTheme>
-        </Providers>
+        <MockUserProvider>
+          <Providers>
+            <StackTheme>
+              <div className="layout">
+                <SideNav />
+                <main className="flex-1">{children}</main>
+              </div>
+              <ClientShell />
+            </StackTheme>
+          </Providers>
+        </MockUserProvider>
       </body>
     </html>
   );
