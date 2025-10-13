f83dfb595ae62340faa85a920697b4c8af498daf
jonathan-kraus <jonathanckraus@gmail.com>
2025-10-13 19:36:03 -0400

    c1

--- app/db.ts
+++ app/db.ts
@@ -1,16 +1,28 @@
 import { neon } from "@neondatabase/serverless";
+import { db } from "./lib/db";
 
+
+console.log("DB module loaded");
 export async function checkDbConnection() {
+  const requestId = crypto.randomUUID();
   if (!process.env.DATABASE_URL) {
     return "No DATABASE_URL environment variable";
   }
   try {
-    const requestId = crypto.randomUUID();
+    await db.log.create({
+  data: {
+    requestId,
+    message: 'dbts checker',
+    severity: 'info',
+    source: 'db.ts',
+  },
+});
+
     const sql = neon(process.env.DATABASE_URL);
     const result = await sql`SELECT version()`;
-    const j = await sql`INSERT INTO public."Log" (requestId, message, severity, source) VALUES (${requestId}, 'Log entry', 'INFO', 'System')`;
+    
     console.log("Pg version:", result);
-    console.log("Insert result:", j);
+
     return "Database connected";
   } catch (error) {
     console.error("Error connecting to the database:", error);
