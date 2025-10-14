671d082a98b6dd77a80fb27d15453523b6b41c41
jonathan-kraus <jonathanckraus@gmail.com>
2025-10-14 18:51:41 -0400

    w2

--- app/lib/fetchWeather.ts
+++ app/lib/fetchWeather.ts
@@ -33,10 +33,11 @@ export async function fetchWeather(requestId?: string) {
   console.log(`[fetchWeather] [${requestId}] Fetching location data from API: ${url2}`);
   const res2 = await fetch(url2);
   if (!res2.ok) throw new Error('Failed to fetch location data');
+  console.log(`[fetchWeather] [${requestId}] Location data fetch response:`, res2);
   const data2 = await res2.json();
   const locationName = data2.address?.city ?? data2.address?.town ?? data2.address?.village ?? data2.address?.hamlet ?? data2.address?.county ?? 'Unknown Location';
   console.log(`[fetchWeather] [${requestId}] Location data fetched from API: locationName`, locationName);
-  
+
   const logEvent = async () => {
     try {
       await fetch('https://kraus.my.id/api/log', {
