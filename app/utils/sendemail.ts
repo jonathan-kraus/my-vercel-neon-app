521d2211ce9a29b840e03dc143f36d9d757fbe3a
jonathan-kraus <jonathanckraus@gmail.com>
2025-10-21 03:01:32 -0400

    log1

--- app/utils/sendemail.ts
+++ app/utils/sendemail.ts
@@ -66,6 +66,7 @@ export async function logEvent(payload: LogPayload) {
 export async function sendConfirmationEmail(toEmail: string, toName: string, requestId?: string) {
   const recipients = [new Recipient(toEmail, toName)];
 
+console.log(`[email-client] sendConfirmationEmail triggered for ${toEmail} with requestId: ${requestId}`);    
   const emailParams = new EmailParams()
     .setFrom(sentFrom)
     .setTo(recipients)
