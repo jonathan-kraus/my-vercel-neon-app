544b4571da14e50f140ab00627548e49a01680b7
jonathan-kraus <jonathanckraus@gmail.com>
2025-10-25 13:26:28 -0400

    t6

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en" className="dark">
       <body className="bg-blue-200 text-[#0C0D0D] dark:bg-black dark:text-white min-h-screen antialiased">
-        <div className="flex h-screen">
+        <div className="flex shrink-0 min-h-screen">
           <SideNav />
           <main className="flex-1">{children}</main>
         </div>
