bccc5f02ae0cf84204ff106eb516547d5dd87ea8
jonathan-kraus <jonathanckraus@gmail.com>
2025-10-26 17:06:58 -0400

    d3

--- app/api/entry/unpublish/route.ts
+++ app/api/entry/unpublish/route.ts
@@ -34,7 +34,7 @@ export async function POST(req: Request) {
 
   try {
     await db.post.update({
-      where: { id },
+      where: { id: Number(id) },
       data: { published: false },
     });
 
