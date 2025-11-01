e82b7b4c255e6aff1216a315458a855cb1a3eb82
jonathan-kraus <jonathanckraus@gmail.com>
2025-11-01 15:18:30 -0400

    center

--- app/admin/weather/page.tsx
+++ app/admin/weather/page.tsx
@@ -81,7 +81,7 @@ export default function WeatherPage() {
 
   return (
     <div className="space-y-4 animate-fade-in">
-      <h2 className="text-xl center font-bold">Weather</h2>
+      <h2 className="text-xl text-center font-bold">Weather</h2>
 
       {forecast.maxRainAccumulation > 0 && (
         <div className="bg-blue-100 border-l-4 border-blue-500 text-blue-700 p-4 rounded">
